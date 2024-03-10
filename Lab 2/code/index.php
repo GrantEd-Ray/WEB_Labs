8591d442c9e38e96fa83135e01f8b480071c75db
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2024-03-10 03:06:35 +0200

    Added task 14

--- Lab 2/code/index.php
+++ Lab 2/code/index.php
@@ -60,3 +60,103 @@ $answer /= 2;
 $answer -= $my_num;
 echo $answer;
 echo "\n <br>";
+
+//task 14 ---------------------------------------------------------------------------------------------------------------
+echo "\n <br>TASK 14 \n <br>";
+
+$a = 10;
+$b = 3;
+echo ($a % $b) . "\n <br>";
+
+if ($a % $b == 0)
+{
+    echo "Делится\n <br>";
+} else
+{
+    echo 'Делится с остатком: ' . ($a % $b) . "\n <br>";
+}
+
+$st = pow(2, 10);
+echo $st . "\n <br>";
+
+echo sqrt(245) . "\n <br>";
+
+$mas = [4, 2, 5, 19, 13, 0, 10];
+$res = 0;
+foreach ($mas as $value)
+{
+    $res += $value;
+}
+echo sqrt($res) . "\n <br>";
+
+echo round(sqrt(379)) . "\n <br>";
+echo round(sqrt(379), 1) . "\n <br>";
+echo round(sqrt(379), 2) . "\n <br>";
+
+$mas1 = ['floor' => floor(sqrt(587)), 'ceil' => ceil(sqrt(587))];
+echo $mas1['ceil'] . ' ' . $mas1['floor'] . "\n <br>";
+
+$mas2 = [4, -2, 5, 19, -130, 0, 10];
+echo min($mas2) . "\n <br>";
+echo max($mas2) . "\n <br>";
+
+echo random_int(1, 100) . "\n <br>";
+
+$randomMas = [];
+while (count($randomMas) < 10)
+{
+    $randomMas[] = random_int(1, 1000);
+}
+
+foreach ($randomMas as $value)
+{
+    echo $value . " ";
+}
+echo "\n <br>";
+
+for ($count = 0; $count < 3; $count++)
+{
+    $a = random_int(-50, 50);
+    $b = random_int(-50, 50);
+    echo $a . ' - ' . $b . ' = ' . (abs($a - $b)) . "\n <br>";
+}
+
+$mas3 = [5, -2, 46, -15, 0, -8, 100];
+foreach($mas3 as &$masValue)
+{
+    $masValue = abs($masValue);
+}
+unset($masValue);
+
+foreach($mas3 as $masValue)
+{
+    echo $masValue . ' ';
+}
+echo "\n <br>";
+
+$thisNum = 86;
+$divs = [1, $thisNum];
+for ($i = 2; $i < ceil(sqrt($thisNum)) + 1; $i++)
+{
+    if ($thisNum % $i == 0)
+    {
+        array_push($divs, $i, $thisNum / $i);
+    }
+}
+
+foreach($divs as $Value)
+{
+    echo $Value . ' ';
+}
+echo "\n <br>";
+
+$someMas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$sum = 0;
+$index = 0;
+
+while ($sum <= 10)
+{
+    $sum += $someMas[$index];
+    $index++;
+}
+echo $index . "\n <br>";
