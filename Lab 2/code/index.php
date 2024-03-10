ac9c42eb639e33a5ff77367552b8be7febca06a4
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2024-03-10 03:04:13 +0200

    Added task 2

--- Lab 2/code/index.php
+++ Lab 2/code/index.php
@@ -9,3 +9,24 @@ $order.= ", only with two bones";
 
 echo "\nYour order is: $very_bad_unclear_name";
 echo "\n <br>";
+
+//task 2 ----------------------------------------------------------------------------------------------------------------
+echo "\n <br>TASK 2 \n <br>";
+
+$itJustWorks = 76;
+echo $itJustWorks . "\n <br>";
+
+$halfLife = 3;
+echo $halfLife . "\n <br>";
+
+$someFloat = 7.052;
+echo "$someFloat\n <br>";
+
+$mods = -64;
+echo $itJustWorks + $mods;
+echo "\n <br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
+echo "\n <br>";
