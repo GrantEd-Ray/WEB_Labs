6b72128d8a967de466fa736946357ca97e614d9b
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2024-03-31 02:22:41 +0200

    Added index.php for task 2 (c)

---
+++ Lab 3/Code/Task 2/Part c/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="page3.php" method="post">
+        <label for="name">Name <input name="name" required></label>
+        <label for="surname">Surname <input name="surname" required></label>
+        <label for="age">Age <input name="age" required></label>
+        <label for="income">Income <input name="income" required></label>
+        <button type="submit" value="save">Save</button>
+    </form>
+</body>
+</html>
